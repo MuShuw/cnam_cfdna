afdb1530abc989607954a14262b6d1c5b42628dd
mushuw <mouhou.elyas@gmail.com>
2022-12-05 11:04:00 +0100

    style editing of src/00_BamToChrScore.py

--- src/00_BamToChrScore.py
+++ src/00_BamToChrScore.py
@@ -7,13 +7,47 @@ import pysam
 import sys
 import time
 
+
 class ChromProcessor(object):
+    """
+    Class used for parallel processing of chromosomes.
+    """
+
     def __init__(self, parameters):
+        """
+        Create the parameter for the chromosomes processing.
+
+        Parameters
+        ----------
+        parameters : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
         self.parameters = parameters
+
     def __call__(self, chromNsize):
+        """
+        Run the function for the chromosomes processing.
+
+        Parameters
+        ----------
+        chromNsize : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+
         chr, size = chromNsize
         data_print(chr, size, self.parameters)
         return
+
 
 def is_valid_file(parser, arg):
     """
@@ -35,6 +69,7 @@ def is_valid_file(parser, arg):
     else:
         return file
 
+
 def is_odd_number(w_size):
     """
     Check if the windows size is an odd number
@@ -45,11 +80,12 @@ def is_odd_number(w_size):
     Returns :
         w_size : (int) size of the windows
     """
-    if w_size %2 == 0:
-        print ("the window size has to be odd!")
+    if w_size % 2 == 0:
+        print("the window size has to be odd!")
         sys.exit()
     else:
         return w_size
+
 
 def get_parser():
     """
@@ -63,7 +99,8 @@ def get_parser():
         Shell
     """
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-    parser = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--input",
                         dest="input",
                         type=lambda x: is_valid_file(parser, x),
@@ -73,7 +110,7 @@ def get_parser():
     parser.add_argument("-s", "--wsize",
                         dest="wind_size",
                         default=121,
-                        type=int ,
+                        type=int,
                         help="size of the windows",
                         required=False)
 
@@ -104,8 +141,8 @@ def get_parser():
                         required=False)
     return parser
 
-def get_chromosomes_names(input):
 
+def get_chromosomes_names(input):
     """
     get the chromosome name and size of each chromosome using the header of the bam
     file
@@ -126,9 +163,12 @@ def get_chromosomes_names(input):
     bamfile.close()
     return list_chromosomes, list_length
 
+
 def count_depth(chr, size, input):
     """
-    Count the depth of the read. For each genomic coordinate return the number
+    Count the read depth.
+
+    For each genomic coordinate return the number
     of reads the array return is a zero base array which means that the index
     begins with 0 instead of 1 for normal genomic coordinates
     -----
@@ -142,7 +182,8 @@ def count_depth(chr, size, input):
         coordinates
     """
     bamfile = pysam.AlignmentFile(input, 'rb')
-    array_depth = np.zeros(size+1,dtype = np.uint16) #WARNING this might reduce the range of the coverage
+    # WARNING this might reduce the range of the coverage
+    array_depth = np.zeros(size+1, dtype=np.uint16)
     for pileupcolumn in bamfile.pileup(chr, stepper='nofilter'):
         pos = pileupcolumn.reference_pos
         depth = pileupcolumn.nsegments
@@ -150,12 +191,8 @@ def count_depth(chr, size, input):
     bamfile.close()
     return array_depth
 
-def parallel_runs(input, list_chromosomes, nb_core):
-    t2 = time.time()
-    tps = t2 - t1
-    print(tps)
 
-def get_wps_and_frag(chr, input, size, w_size,frag_min,frag_max):
+def get_wps_and_frag(chr, input, size, w_size, frag_min, frag_max):
     """
      Compute the WPS(Windows Protection Score) and  Get the size, start and
      stop coordinates for each fragment
@@ -175,8 +212,9 @@ def get_wps_and_frag(chr, input, size, w_size,frag_min,frag_max):
         for each genomic coordinates
     """
     bamfile = pysam.AlignmentFile(input, 'rb')
-    frag_overlap = np.zeros(size+1,dtype = np.uint16) #WARNING this might reduce the range of the coverage
-    wps_array = np.zeros(size+1,dtype = np.int16)
+    # WARNING this might reduce the range of the coverage
+    frag_overlap = np.zeros(size+1, dtype=np.uint16)
+    wps_array = np.zeros(size+1, dtype=np.int16)
     for read in bamfile.fetch(contig=chr):
         if read.is_proper_pair:
             if read.mate_is_unmapped:
@@ -195,16 +233,20 @@ def get_wps_and_frag(chr, input, size, w_size,frag_min,frag_max):
                 frag_start = min(list_read)
                 frag_end = max(list_read)
                 frag_overlap[frag_start:frag_end] += 1
-                wps_array[int(frag_start -(w_size-1)/2):int(frag_start+flen+(w_size-1)/2+1)] -=1
+                wps_array[int(frag_start - (w_size-1)/2):
+                          int(frag_start+flen+(w_size-1)/2+1)] -= 1
                 if (flen >= w_size):
-                    wps_array[int(frag_start +(w_size-1)/2):int(frag_start+flen-(w_size-1)/2)+1] +=2
+                    wps_array[int(frag_start + (w_size-1)/2):
+                              int(frag_start+flen-(w_size-1)/2)+1] += 2
     bamfile.close()
-    return frag_overlap,wps_array
+    return frag_overlap, wps_array
+
 
 def data_print(chr, size, parameters):
     """
      Gathering of all the score compute in order to prepare an output with a
-     TSV file format
+     TSV file format.
+
     -----
     Parameters :
         chr : (str) name of the chromosome
@@ -218,19 +260,22 @@ def data_print(chr, size, parameters):
 
     """
     input, w_size, frag_min, frag_max = parameters
-    timer1 = time.time()
     print(chr + "  is processing")
     out_name = input.split("/")[-1].split(".")[0] + "_" + chr
 
     depth_array = count_depth(chr, size, input)
-    frag_array,wps_array = get_wps_and_frag(chr, input, size,w_size,frag_min,frag_max)
+    frag_array, wps_array = get_wps_and_frag(
+        chr, input, size, w_size, frag_min, frag_max)
     t1 = time.time()
-    depth_array,frag_array,wps_array = depth_array[1:,],frag_array[1:,],wps_array[1:,]
-    out_matrix = np.rec.fromarrays([depth_array,frag_array,wps_array], names='rd,fd,wps')
+    depth_array, frag_array, wps_array = depth_array[1:, ],
+    frag_array[1:, ], wps_array[1:, ]
+    out_matrix = np.rec.fromarrays(
+        [depth_array, frag_array, wps_array], names='rd,fd,wps')
     np.savez_compressed(out_name, out_matrix)
     t2 = time.time()
-    total_time = round((t2 - timer1)/60)
-    print("total time for {} is {} min".format(chr,total_time))
+    total_time = round((t2 - t1)/60)
+    print("total time for {} is {} min".format(chr, total_time))
+
 
 def get_stat(input):
     """
@@ -247,23 +292,24 @@ def get_stat(input):
     bamfile.close()
     return stat
 
+
 if __name__ == "__main__":
     args = get_parser().parse_args()
     print("input file : "+args.input)
     print("windows size is "+str(args.wind_size)+" bp")
-    print("fragments used for WPS is between {} and {} bp".format(str(args.frag_min),str(args.frag_max)))
-    print("number of core used : {}".format (str(args.nb_core)))
+    print("fragments used for WPS is between {} and {} bp".format(
+        str(args.frag_min), str(args.frag_max)))
+    print("number of core used : {}".format(str(args.nb_core)))
     list_chromosomes, list_length = get_chromosomes_names(args.input)
     is_odd_number(args.wind_size)
     # marche avec un parametres
     # test avec deux parametres
-    # parallel_runs(args.input,list_chromosomes,list_length,args.nb_core)
-    try :
+    try:
         #pool = Pool(os.cpu_count() - 1)
         pool = Pool(23)
-        dataProcessor = ChromProcessor((args.input, args.wind_size, args.frag_min, args.frag_max))
+        dataProcessor = ChromProcessor(
+            (args.input, args.wind_size, args.frag_min, args.frag_max))
         pool.map(dataProcessor, zip(list_chromosomes, list_length))
     finally:
         pool.close()
         pool.join()
-
