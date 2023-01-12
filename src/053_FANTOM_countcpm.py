9977b995bd2bb06c5b3bc1ce08724b0aa85d0265
Josselin Noirel <josselinnoirel@gmail.com>
2023-01-12 17:22:46 +0100

    Typos in script

--- src/053_FANTOM_countcpm.py
+++ src/053_FANTOM_countcpm.py
@@ -5,12 +5,12 @@ import numpy as np
 import pandas as pd
 
 # First get the read count for FANTOMv5
-df = pd.read_csv('../../data/expression/FANTOM/FANTOM_CAT.expression_atlas.gene.lv3_robust.count.tsv.gz', sep='\t', index_col=0)
+df = pd.read_csv('data/expression/FANTOM/FANTOM_CAT.expression_atlas.gene.lv3_robust.count.tsv.gz', sep='\t', index_col=0)
 
 sample_depth = dt.apply(sum, axis=0)
 factor_ratio = sample_depth/1_000_000
 
 tpm_df = df/factor_ratio
 
-tpm_df.to_csv('../../results/FANTOM_CAT.expression_atlas.gene.lv3_robust.tpm_ELYAS.tsv', sep='\t')
+tpm_df.to_csv('results/FANTOM_CAT.expression_atlas.gene.lv3_robust.tpm_ELYAS.tsv', sep='\t')
 
--- src/054_FANTOM_groupingPerOntho.py
+++ src/054_FANTOM_groupingPerOntho.py
@@ -13,16 +13,16 @@ def get_mean_for_ontho(pd_serie):
     return pd.Series(tmp_mean, name = pd_serie[1])
 
 MYREGEX = re.compile('[ ,-]')
-onthology_sample_association = pd.read_csv('../../data/expression/FANTOM/supp/supp_table_10.sample_ontology_information.csv', sep='\t', header=0)
+onthology_sample_association = pd.read_csv('data/expression/FANTOM/supp/supp_table_10.sample_ontology_information.csv', sep='\t', header=0)
 real_ontho = onthology_sample_association.apply( lambda x : '_'.join([*x[0].split(':'), *MYREGEX.split(x[1])]), axis=1)
 onthology_sample_association=onthology_sample_association.assign(real_ontho=real_ontho)
 onthology_sample_association=onthology_sample_association[['CAGE_lib_ID','real_ontho']]   
 
-tpm_sample = pd.read_csv('../../results/FANTOM_CAT.expression_atlas.gene.lv3_robust.tpm_ELYAS.tsv',sep='\t', index_col=0)
+tpm_sample = pd.read_csv('results/FANTOM_CAT.expression_atlas.gene.lv3_robust.tpm_ELYAS.tsv',sep='\t', index_col=0)
 
 x_tmp = onthology_sample_association.apply( get_mean_for_ontho, axis=1)
 x_tmp.index=onthology_sample_association.real_ontho
 x_tmp=np.transpose(x_tmp)
 
-x_tmp.to_csv('../../results/FANTOM_CAT.expression_atlas.gene.lv3_robust.MEDIAN_tpm_PER_ONTHO_ELYAS.tsv', sep='\t')
+x_tmp.to_csv('results/FANTOM_CAT.expression_atlas.gene.lv3_robust.MEDIAN_tpm_PER_ONTHO_ELYAS.tsv', sep='\t')
 
